424f748e6e17f2e1ffb71961fb8385ba3b9bf407
theolukayodeasemudara <blaqnativity@gmail.com>
2024-05-08 07:48:58 +0100

    new commit

--- components/transaction.vue
+++ components/transaction.vue
@@ -1,5 +1,19 @@
 <script setup>
 const { currency } = useCurrency(3000);
+const items = [
+  [
+    {
+      label: "Edit",
+      icon: "i-heroicons-pencil-square",
+      click: () => console.log("Edit"),
+    },
+    {
+      label: "Delete",
+      icon: "i-heroicons-trash-tv-20-solid",
+      click: () => console.log("Delete"),
+    },
+  ],
+];
 </script>
 
 <template>
@@ -15,7 +29,20 @@ const { currency } = useCurrency(3000);
     </div>
     <div class="flex items-center justify-end space-x-2">
       <div>{{ currency }}</div>
-      <div><button>Options</button></div>
+      <div>
+        <UDropdown
+          :items="items"
+          mode="click"
+          :popper="{ placement: 'bottom-start' }"
+        >
+          <UButton
+            color="white"
+            variant="ghost"
+            label="Options"
+            trailing-icon="i-heroicons-ellipsis-horizontal"
+          />
+        </UDropdown>
+      </div>
     </div>
   </div>
 </template>
