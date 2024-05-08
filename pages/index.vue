43acfc4fe0d84063b8ec293114098759ea392f99
theolukayodeasemudara <blaqnativity@gmail.com>
2024-05-08 07:22:06 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { transactionsViewOptions } from "~/constants";
+const selected = ref(transactionsViewOptions[0]);
+</script>
+
+<template>
+  <section class="flex items-center justify-between mb-10">
+    <h1 class="text-4xl font-extrabold">Summary</h1>
+    <div>
+      <USelectMenu v-model="selected" :options="transactionsViewOptions" />
+    </div>
+  </section>
+
+  <section
+    class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10"
+  >
+    <Trend
+      color="green"
+      title="Income"
+      :amount="4000"
+      :last-amount="3000"
+      :loading="false"
+    />
+    <Trend
+      color="red"
+      title="Expenses"
+      :amount="4000"
+      :last-amount="5000"
+      :loading="false"
+    />
+    <Trend
+      color="green"
+      title="Investments"
+      :amount="4000"
+      :last-amount="3000"
+      :loading="false"
+    />
+    <Trend
+      color="red"
+      title="Savings"
+      :amount="4000"
+      :last-amount="4100"
+      :loading="false"
+    />
+  </section>
+</template>
